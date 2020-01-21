44e06bb8ae327255291a1f8356088d91c9c7dd97
sethbaldridge87 <sethbaldridge87@gmail.com>
2020-01-21 12:37:31 -0700

    Animated SVG loader added. Logo added to nav

--- js/script.js
+++ js/script.js
@@ -1,8 +1,21 @@
 $(window).on('load', function() {
-    console.log('window ready');
-    $('#loader').fadeOut('slow');
+    // $('#loader').fadeOut('slow');
   });
 
 $(document).ready(function(){
-    console.log('document ready');
+  var path = document.querySelectorAll('path');
+  var totalPaths = path.length;
+  var y = 1;
+  for (var i = 0; i < totalPaths; i++) {
+      var length = path[i].getTotalLength();
+      $('path:nth-of-type(' + y + ')').css('fill','rgb(238, 206, 238)');
+      $('path:nth-of-type(' + y + ')').css('stroke','#000');
+      $('path:nth-of-type(' + y + ')').css('stroke-dasharray',length);
+      $('path:nth-of-type(' + y + ')').css('stroke-dashoffset',length);
+      y++;
+  }
+  setTimeout(function(){
+      $('path').css('transition','4s');
+      $('path').css('stroke-dashoffset', 0);
+  }, 1);
 });

c6c89a68ea79ae0420c73a1169db68079d723bf4
sethbaldridge87 <sethbaldridge87@gmail.com>
2020-01-25 20:00:24 -0700

    Image gallery function added

---
+++ js/character.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+    var gallImg = [];
+    $('#character-images img').each(function(){
+        var imgSrc = $(this).attr('src');
+        gallImg.push(imgSrc);
+    });
+    $('#character-images img').click(function(){
+        var counter;
+        var image = $(this).attr('src');
+        $('#loader').css({display:'block',background:'rgb(103,188,214,.75)'});
+        var gallery = $('<div class="gallery"><img src="' + image + '" /><div id="left" class="arrow"><</div><div id="right" class="arrow">></div><div id="close">x</div></div>');
+        $('#loader').html(gallery);
+        for (i = 0; i < gallImg.length; i++) {
+            if (gallImg[i] == image) {
+                counter = i;
+            }
+        }
+        $('.arrow').on('click', function(){
+            if ($(this).attr('id') === 'right') {
+                counter = increase(counter);
+            } else if ($(this).attr('id') === 'left') {
+                counter = decrease(counter);
+            }
+            $('.gallery img').attr('src',gallImg[counter]);
+        });
+        $('#close').on('click', function(){
+            $('#loader').css('display','none');
+        });
+        function increase(num) {
+            num++;
+            if (num > gallImg.length - 1) {
+                num = 0;
+            }
+            return num;
+        }
+        function decrease(num) {
+            num--;
+            if (num == -1) {
+                num = gallImg.length - 1;
+            }
+            return num;
+        }
+    });
+});
--- js/script.js
+++ js/script.js
@@ -1,21 +1,3 @@
 $(window).on('load', function() {
-    $('#loader').fadeOut('slow');
-  });
-
-$(document).ready(function(){
-  // var path = $('#loader path');
-  // var totalPaths = path.length;
-  // var y = 1;
-  // for (var i = 0; i < totalPaths; i++) {
-  //     var length = path[i].getTotalLength();
-  //     $('#loader path:nth-of-type(' + y + ')').css('fill','rgb(238, 206, 238)');
-  //     $('#loader path:nth-of-type(' + y + ')').css('stroke','rgb(68, 67, 67)');
-  //     $('#loader path:nth-of-type(' + y + ')').css('stroke-dasharray',length);
-  //     $('#loader path:nth-of-type(' + y + ')').css('stroke-dashoffset',length);
-  //     y++;
-  // }
-  // setTimeout(function(){
-  //     $('#loader path').css('transition','4s');
-  //     $('#loader path').css('stroke-dashoffset', 0);
-  // }, 1);
+  $('#loader').fadeOut('slow');
 });
